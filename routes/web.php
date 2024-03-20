d43bb667e5d663ecd0a264e2fcf711dab55fb6f8
andre lopes <andre.sousa70@etec.sp.gov.br>
2024-03-20 01:09:13 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@ Route::get('/{name}/{lastname?}',[useController::class, 'andre']);
 Route::get('/{num1}/{op}/{num2}',[useController::class, 'calculo']);
 
 Route::get('/{name1}/{lastname1?}/{age?}/{gender?}/{email?}/{telephone?}',[useController::class, 'dados']);
+
+Route::get('/{descption}/{category}/{cost}',[useController::class, 'product']);
